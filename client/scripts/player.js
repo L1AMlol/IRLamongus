2d26b81dad40d33992c1ec555ecb2df3d5d6ab22
L1AMlol <liam.eggermont@gmail.com>
2025-09-06 06:40:55 +0200

    accedentaly made a chatroom lol

---
+++ client/scripts/player.js
@@ -0,0 +1,54 @@
+const testBtn = document.getElementById("testBtn");
+const messageInput = document.getElementById("messageInput");
+
+const sender = Date.now + Math.floor(Math.random() * (106030200));
+
+const socket = new WebSocket("ws://localhost:8765");
+
+const sendPayload = function (payload) {
+    socket.send(JSON.stringify(payload));
+}
+
+socket.onopen = () => {
+    console.log('Connected to WebSocket server');
+    const payload = {
+        message: "first from host",
+        userType: "player",
+        messageType: "auth",
+        sender: sender,
+    };
+    sendPayload(payload);
+};
+
+// When the server sends a message back
+socket.onmessage = (message) => {
+    console.log('Received message from server: \n', message.data);
+
+    // if (message.data === 'ping') {
+    //     console.log('Received ping, sending pong');
+    //     socket.send('pong');
+    // }
+};
+
+// If there is an error in the WebSocket connection
+socket.onerror = (error) => {
+    console.error('WebSocket Error:', error);
+};
+
+// When the connection is closed
+socket.onclose = e => {
+    console.log('WebSocket connection closed');
+};
+
+testBtn.addEventListener('click', () => {
+    if(!messageInput.value){ return }
+    const message = messageInput.value;
+    const payload = {
+        message: message,
+        userType: "player",
+        messageType: "normal",
+        sender: sender,
+    };
+    sendPayload(payload);
+});
+
--- server/server.py
+++ server/server.py
@@ -1,26 +1,59 @@
 import asyncio
 import websockets
+import json
 
-connected_clients = set()
+hosts = set()
+players = set()
 
 async def handle_client(ws):
-    connected_clients.add(ws)
-    print(f"New client connected: {ws.remote_address}")
+    
     try:
+        first_msg_str = await ws.recv()
+        first_msg = json.loads(first_msg_str)
+        if first_msg['userType'] == "host":
+            if ws not in hosts:
+                hosts.add(ws)
+                # ToDo: add class host
+                is_host = True
+                is_player = False
+                print("a host has connected")
+            
+        elif first_msg['userType'] == "player":
+            if ws not in players:
+                players.add(ws)
+                # ToDo: add class player
+                is_host = False
+                is_player = True
+                print("a player has connected")
         while True:
-            payload = await ws.recv()
-            print(payload)
+            payload_str = await ws.recv()
+            payload = json.loads(payload_str)
+            if is_host:
+                print(f"host sent: {payload['message']}")
+            elif is_player:
+                print(f"player sent: {payload['message']}")
+
 
     except websockets.exceptions.ConnectionClosed:
-        print(f"Client {ws.remote_address} disconnected")
+        if ws in hosts:
+            print("a host has disconnected")
+        elif ws in players:
+            print("a player has disconnected")
 
     finally:
-        connected_clients.remove(ws)
+        if ws in hosts:
+            hosts.remove(ws)
+        elif ws in players:
+            players.remove(ws)
+        
 
 async def broadcast_ping():
     while True:
-        if connected_clients:
-            for ws in connected_clients:
+        if hosts:
+            for ws in hosts:
+                await ws.ping()
+        if players:
+            for ws in players:
                 await ws.ping()
         await asyncio.sleep(5)
 
